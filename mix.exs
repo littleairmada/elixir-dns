2e3c2ea7f7f1b1f5bb37d34e7871c43baf86ffcd
littleairmada <littleairmada@users.noreply.github.com>
2022-12-18 16:04:16 -0500

    update credo dep; ignore Credo.Check.Readability.LargeNumbers check for port numbers (65535 or smaller)

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule DNS.Mixfile do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false},
-      {:credo, "~> 0.9.0-rc1", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
